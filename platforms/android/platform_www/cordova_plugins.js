f3f9a9e7818f9e98398d78c1902afdbf743c89aa
stevenpro8x <steven.bb8700iii@gmail.com>
2016-07-05 23:34:43 +0700

    test hello1
    test hello1

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-admobpro/www/AdMob.js",
+        "id": "cordova-plugin-admobpro.AdMob",
+        "clobbers": [
+            "window.AdMob"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-extension": "1.5.1",
+    "cordova-plugin-admobpro": "2.18.0"
+};
+// BOTTOM OF METADATA
+});
